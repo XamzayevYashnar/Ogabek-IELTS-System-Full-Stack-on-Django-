ec33e5fa0bbd8c6426197f30156c9aa88a06a444
yashnar <xamzayevyashnar060@gmail.com>
2026-07-22 13:49:01 +0500

    optimizate config folder on urls.py delete not need modules!

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from materials.views import IndexView
-from django.urls import path, include, re_path
-from django.conf import settings
+from django.urls import path, re_path
 from django.views.static import serve
 
 urlpatterns = [
