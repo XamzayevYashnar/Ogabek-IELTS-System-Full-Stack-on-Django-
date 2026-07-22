d24bf42f0074963601b369e26fa7de3374f3adc6
yashnar <xamzayevyashnar060@gmail.com>
2026-07-22 12:35:09 +0500

    production media fayllar sozlandi

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,17 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-
 from materials.views import IndexView
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
